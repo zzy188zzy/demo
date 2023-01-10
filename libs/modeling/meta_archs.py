fd058f25c74d16d1fd4f02cb362c94a14843e295
fy-coder <2482312879@qq.com>
2023-01-10 17:25:32 +0800

    a change

--- libs/modeling/meta_archs.py
+++ libs/modeling/meta_archs.py
@@ -597,6 +597,7 @@ class PtTransformer(nn.Module):
         masks = []
         t = 1
         for i, (cls_i, mask) in enumerate(zip(out_cls_logits, fpn_masks)):
+            mask = mask==False
             print(cls_i)
             cls_i = torch.softmax(cls_i, dim=2)
             print(cls_i)
@@ -611,7 +612,7 @@ class PtTransformer(nn.Module):
             scores.append(cls_i)
             masks.append(mask)
             t *= 2
-            print('-------')
+            print('---------------------------------------------------------------')
         
         scores = torch.stack(scores,dim=1)  # [2, 6, 2304]
         # print(scores)
