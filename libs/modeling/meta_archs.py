ef832468b6b798fa5abd1ffacb96a6a3395fe2f1
fy-coder <2482312879@qq.com>
2023-01-10 19:25:57 +0800

    a change

--- libs/modeling/meta_archs.py
+++ libs/modeling/meta_archs.py
@@ -639,10 +639,12 @@ class PtTransformer(nn.Module):
 
         scores -= torch.ones(scores.shape, device=scores.device)*0.05  # 0.05
 
-        level = 1
+        level = 3
         scores = scores[:, :level, :]
 
         sco_loss = scores.sum() / (level*2304*scores.shape[0])
+
+        sco_loss *= 10
         
         # sco_loss /= idx.sum()
 
@@ -653,7 +655,7 @@ class PtTransformer(nn.Module):
             loss_weight = cls_loss.detach() / max(reg_loss.item(), 0.01)
 
         # return a dict of losses
-        final_loss = cls_loss + reg_loss * loss_weight + sco_loss * 10
+        final_loss = cls_loss + reg_loss * loss_weight + sco_loss
         return {'cls_loss'   : cls_loss,
                 'reg_loss'   : reg_loss,
                 'sco_loss'   : sco_loss,
