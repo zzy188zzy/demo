840f5fa5cc6f6b49e550708600d9cb3c87860d81
fy-coder <2482312879@qq.com>
2023-01-15 16:10:48 +0800

    a change

--- libs/modeling/meta_archs.py
+++ libs/modeling/meta_archs.py
@@ -695,7 +695,7 @@ class PtTransformer(nn.Module):
         var_C = torch.var(ft_C, axis=0)
         log_var_C = torch.log(var_C + 1e-6)
         loss_KL = torch.mean(mean_C*mean_C + var_C - log_var_C - 1) / 2
-        loss = ((loss_S + loss_D)+0.001*loss_KL)
+        loss = ((loss_S + loss_D)+0.0001*loss_KL)
         # loss = loss_S + loss_D
         return (loss / max(L, 1)) * T
 
