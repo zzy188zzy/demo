b0a9f8c8e8345505c1cd4c249752a0ed386e8626
fy-coder <2482312879@qq.com>
2023-01-16 14:10:40 +0800

    a change

--- libs/modeling/meta_archs.py
+++ libs/modeling/meta_archs.py
@@ -783,7 +783,7 @@ class PtTransformer(nn.Module):
         masks = torch.stack(masks,dim=1)
 
         idx = torch.sum(masks, dim=1)
-        idx = idx < 5
+        idx = idx < 6
         
         low = torch.min(scores, dim=1).values
         scores[masks]=0
