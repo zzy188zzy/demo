5b2a03a64b2cf4ca2c8d7cbadf7b19f4ec4fe6c8
fy-coder <2482312879@qq.com>
2023-01-20 11:42:45 +0800

    a change

--- libs/modeling/meta_archs.py
+++ libs/modeling/meta_archs.py
@@ -1226,10 +1226,10 @@ class PtTransformer(nn.Module):
             # print(seg_left[left_mask])
             
 
-            # ref_left = out_refines[left_idx[left_mask]]  # todo [2304]
-            # seg_left[left_mask] += ref_left
-            # ref_right = out_refines[right_idx[right_mask]]  # todo [2304]
-            # seg_right[right_mask] += ref_right
+            ref_left = out_refines[left_idx[left_mask]]  # todo [2304]
+            seg_left[left_mask] += ref_left
+            ref_right = out_refines[right_idx[right_mask]]  # todo [2304]
+            seg_right[right_mask] += ref_right
 
 
             # print(ref_left)
