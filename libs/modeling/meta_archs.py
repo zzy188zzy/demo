c0a609c5fc7cdb67b5b1ee66c125fc62da213a52
fy-coder <2482312879@qq.com>
2023-01-23 13:13:50 +0800

    a change

--- libs/modeling/meta_archs.py
+++ libs/modeling/meta_archs.py
@@ -1210,6 +1210,7 @@ class PtTransformer(nn.Module):
         for i, (cls_i, offsets_i, ref_i, pts_i, mask_i) in enumerate(zip(
                 out_cls_logits, out_offsets, out_refines, points, fpn_masks
             )):
+            ref_i = ref_i[0]
             # sigmoid normalization for output logits
             pred_prob = (cls_i.sigmoid() * mask_i.unsqueeze(-1)).flatten()
 
