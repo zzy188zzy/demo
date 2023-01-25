29ce6653e0d37a66e1c762e207ac2ec73ce29884
fy-coder <2482312879@qq.com>
2023-01-25 12:56:50 +0800

    a change

--- libs/modeling/meta_archs.py
+++ libs/modeling/meta_archs.py
@@ -758,7 +758,7 @@ class PtTransformer(nn.Module):
         print(dis0)
         print(dis0.shape)
         print(dis.shape)
-        idx = dis[lis[:, None].repeat(1, 2), dis_idx0]<0
+        idx = dis[lis[:, None].repeat(1, num_gts), dis_idx0]<0
         print(idx.shape)
         dis0[idx] *= -1
         print(dis0)
