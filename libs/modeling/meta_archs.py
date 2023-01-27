cb8f120cb78f4279183515347744cbbe295e0f4a
fy-coder <2482312879@qq.com>
2023-01-27 13:12:32 +0800

    a change

--- libs/modeling/meta_archs.py
+++ libs/modeling/meta_archs.py
@@ -1340,8 +1340,8 @@ class PtTransformer(nn.Module):
             seg_right = pts[:, 0] + offsets[:, 1] * pts[:, 3]
 
             use_round = True
-            if i!=0 :
-            # if True:
+            # if i!=0 :
+            if False:
                 # 1 2 3 4 5
                 a = [1,2,4,8,16]
                 stride_i = a[i-1]
