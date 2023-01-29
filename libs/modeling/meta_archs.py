2ceb9df6afa7be8741a01076593c33832bc941c5
fy-coder <76418928+fy-coder@users.noreply.github.com>
2023-01-29 21:11:34 +0800

    Update meta_archs.py

--- libs/modeling/meta_archs.py
+++ libs/modeling/meta_archs.py
@@ -1361,13 +1361,13 @@ class PtTransformer(nn.Module):
             use_round = True
             use_prob = False
             # if i!=0 and i!=1 :
-            if i!=0 :
+#             if i!=0 :
 #             if False:
-            # if True:
+            if True:
                 # 1 2 3 4 5
-                a = [1,2,4,8,16]
-                b = -1
-                c = 4
+                a = [1,2,4,8,16,32]
+                b = 0
+                c = 2
                 d = 80
                 e = 1
                 stride_i = a[i+b]
