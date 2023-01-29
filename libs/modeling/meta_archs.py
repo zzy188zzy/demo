ff217c1f35ccb63d366ab66bc16bea6c6e7367bd
fy-coder <76418928+fy-coder@users.noreply.github.com>
2023-01-29 13:21:48 +0800

    Update meta_archs.py

--- libs/modeling/meta_archs.py
+++ libs/modeling/meta_archs.py
@@ -466,7 +466,7 @@ class PtTransformer(nn.Module):
 
         # classfication and regerssion heads
         self.cls_head = PtTransformerClsHead(
-            fpn_dim*2, head_dim, self.num_classes,
+            fpn_dim, head_dim, self.num_classes,
             kernel_size=head_kernel_size,
             prior_prob=self.train_cls_prior_prob,
             with_ln=head_with_ln,
@@ -489,7 +489,7 @@ class PtTransformer(nn.Module):
         self.relu = nn.ReLU()
 
         self.refineHead = RefineHead(
-            fpn_dim, head_dim, len(self.fpn_strides),
+            fpn_dim*2, head_dim, len(self.fpn_strides),
             kernel_size=head_kernel_size,
             num_layers=head_num_layers,
             with_ln=head_with_ln
@@ -536,9 +536,11 @@ class PtTransformer(nn.Module):
         points = self.point_generator(fpn_feats)
 
         # out_cls: List[B, #cls + 1, T_i]
-        out_cls_logits = self.cls_head(cat_feats, fpn_masks)
+        out_cls_logits = self.cls_head(fpn_feats, fpn_masks)
+#         out_cls_logits = self.cls_head(cat_feats, fpn_masks)
         # out_offset: List[B, 2, T_i]
         out_offsets = self.reg_head(fpn_feats, fpn_masks)
+#         out_offsets = self.reg_head(cat_feats, fpn_masks)
 
         # # permute the outputs
         # # out_cls: F List[B, #cls, T_i] -> F List[B, T_i, #cls]
@@ -547,12 +549,15 @@ class PtTransformer(nn.Module):
         # out_offsets = [x.permute(0, 2, 1) for x in out_offsets]
         # # fpn_masks: F list[B, 1, T_i] -> F List[B, T_i]
         # fpn_masks = [x.squeeze(1) for x in fpn_masks]
-
+        
+        
+#         out_refines, out_probs = self.refineHead(fpn_feats0, fpn_masks0)
+        out_refines, out_probs = self.refineHead(cat_feats, fpn_masks)
+        
         # return loss during training
         if self.training:
             # train refineHead
-            out_refines, out_probs = self.refineHead(fpn_feats0, fpn_masks0)
-#             out_refines, out_probs = self.refineHead(cat_feats, fpn_masks)
+            
 
             # permute the outputs
             # out_cls: F List[B, #cls, T_i] -> F List[B, T_i, #cls]
@@ -625,7 +630,7 @@ class PtTransformer(nn.Module):
             #     print(out_cls_logits[i].shape)
             # exit()
 
-            out_refines, out_probs = self.refineHead(fpn_feats0, fpn_masks0)
+#             out_refines, out_probs = self.refineHead(fpn_feats0, fpn_masks0)
 #             out_refines, out_probs = self.refineHead(cat_feats, fpn_masks)
 
             # permute the outputs
