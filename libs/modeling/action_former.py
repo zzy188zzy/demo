8f692d1a4a1eeecf501c5fecede5b950623f5c27
fy-coder <2482312879@qq.com>
2023-02-02 03:48:20 +0800

    a change

--- libs/modeling/action_former.py
+++ libs/modeling/action_former.py
@@ -1025,11 +1025,11 @@ class PtTransformer(nn.Module):
                     b = 1
                     c = 1
                     e = 1
-                    stride_i = a[max(i+b, 5)]
-                    for j in range(max(i+b+1, 6)):  # 1 2 3 4 5 6
+                    stride_i = a[min(i+b, 5)]
+                    for j in range(min(i+b+1, 6)):  # 1 2 3 4 5 6
                         # 1 2 4 8 16 32
-                        ref = out_refines[max(i+b, 5)-j].squeeze(1)
-                        stride_j = a[max(i+b, 5)-j]
+                        ref = out_refines[min(i+b, 5)-j].squeeze(1)
+                        stride_j = a[min(i+b, 5)-j]
 
                         assert stride_i == stride_j
                     
