2282ebb8d61938f5f5ece9a31a8b5693d3d4825e
fy-coder <2482312879@qq.com>
2023-02-02 01:23:04 +0800

    a change

--- libs/modeling/action_former.py
+++ libs/modeling/action_former.py
@@ -1023,7 +1023,7 @@ class PtTransformer(nn.Module):
                     # 1 2 3 4 5
                     a = [1,2,4,8,16,32]
                     b = -1
-                    c = 3
+                    c = 2
                     e = 1
                     stride_i = a[i+b]
                     for j in range(i+b+1):  # 1 2 3 4 5 6
