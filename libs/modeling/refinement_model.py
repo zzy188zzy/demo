2166e24b18a584d26c0e97c16b1e6a401269bf8a
fy-coder <2482312879@qq.com>
2023-02-02 19:07:20 +0800

    a change

--- libs/modeling/refinement_model.py
+++ libs/modeling/refinement_model.py
@@ -367,7 +367,7 @@ class Refinement_module(nn.Module):
 
         t = 0
         for ref_i in out_refines:
-            ref_i = ref_i[0]
+            ref_i = ref_i[:]
             print(ref_i.shape)
             B, T, C = ref_i.shape
             gt_i = gt_ref_low[:, t:t+T, :]
