abf0067134253cc6f52777932329914af22798be
fy-coder <2482312879@qq.com>
2023-02-02 23:08:29 +0800

    a change

--- libs/core/config.py
+++ libs/core/config.py
@@ -43,7 +43,7 @@ DEFAULTS = {
         # type of backbone (convTransformer | conv)
         "backbone_type": 'conv',
         # type of FPN (fpn | identity)
-        "fpn_type": "identity",
+        "fpn_type": "fpn",
         "backbone_arch": (2, 2, 5),
         # scale factor between pyramid levels
         "scale_factor": 2,
