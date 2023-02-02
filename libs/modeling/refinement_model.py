fc9706934542b76221a39f07261388fc5ccdd7fa
fy-coder <2482312879@qq.com>
2023-02-03 05:16:54 +0800

    a change

--- libs/modeling/refinement_model.py
+++ libs/modeling/refinement_model.py
@@ -402,8 +402,8 @@ class Refinement_module(nn.Module):
         mask = torch.logical_and((outside == False), valid_mask[:, :, None].repeat(1, 1, 2))
         out_mask = torch.logical_and((outside == True), valid_mask[:, :, None].repeat(1, 1, 2))
 
-        inf_loss = F.smooth_l1_loss(out_ref[out_mask], out_ref[out_mask]*0, reduction='mean')
-        # inf_loss = torch.abs(out_ref[out_mask]).mean()
+        # inf_loss = F.smooth_l1_loss(out_ref[out_mask], out_ref[out_mask]*0, reduction='mean')
+        inf_loss = torch.abs(out_ref[out_mask]).mean()
 
         gt_low = gt_low[mask]
         out_ref = out_ref[mask]
