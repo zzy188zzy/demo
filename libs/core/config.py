45ee8ccf29cd64a2f438a9408ae0c1037200654c
fy-coder <2482312879@qq.com>
2023-02-03 02:14:10 +0800

    a change

--- libs/core/config.py
+++ libs/core/config.py
@@ -41,7 +41,7 @@ DEFAULTS = {
     # network architecture
     "model": {
         # type of backbone (convTransformer | conv)
-        "backbone_type": 'convTransformer',
+        "backbone_type": 'conv',
         # type of FPN (fpn | identity)
         "fpn_type": "identity",
         "backbone_arch": (2, 2, 5),
