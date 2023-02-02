c8210556950a173d3c5e5b8ac3ee282190200ae4
fy-coder <2482312879@qq.com>
2023-02-03 04:36:17 +0800

    a change

--- libs/modeling/action_former.py
+++ libs/modeling/action_former.py
@@ -1016,7 +1016,7 @@ class PtTransformer(nn.Module):
                 seg_left = pts[:, 0] - offsets[:, 0] * pts[:, 3]
                 seg_right = pts[:, 0] + offsets[:, 1] * pts[:, 3]
 
-                use_round = True
+                use_round = False
                 # if i!=0 and i!=1 :
                 # if i!=0 :
                 if True:
--- libs/modeling/refinement_model.py
+++ libs/modeling/refinement_model.py
@@ -206,7 +206,7 @@ class Refinement_module(nn.Module):
                 # c_loss.append(loss['c_loss'])
 
             ref_loss = torch.stack(ref_loss).min()
-            inf_loss = torch.stack(inf_loss).min()
+            inf_loss = torch.stack(inf_loss).min() * 10
             # c_loss = torch.stack(c_loss).mean()
             final_loss = ref_loss + inf_loss
 
@@ -304,8 +304,8 @@ class Refinement_module(nn.Module):
         gt_ref_low = dis0.clone()
         gt_ref_high = dis0.clone()
 
-        low_p = 0.1  # 0 ~ 1
-        high_p = 0.1
+        low_p = 0  # 0 ~ 1
+        high_p = 0
 
         ra = concat_points[:, 1]
         rb = concat_points[:, 2]
