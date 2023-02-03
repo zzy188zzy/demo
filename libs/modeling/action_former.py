8adf14c908dddc273f4898f0ee9971582532f85f
fy-coder <76418928+fy-coder@users.noreply.github.com>
2023-02-04 04:14:48 +0800

    Update action_former.py

--- libs/modeling/action_former.py
+++ libs/modeling/action_former.py
@@ -1030,7 +1030,7 @@ class PtTransformer0(nn.Module):
                 # seg_areas = seg_right - seg_left
                 # keep_idxs20 = seg_areas > self.test_duration_thresh
 
-                # pred_prob_len = max(pred_prob.max()-pred_prob.min(), 0.5)
+                pred_prob_len = max(pred_prob.max()-pred_prob.min(), 0.5)
 
                 use_round = True
                 # if i!=0 and i!=1 :
@@ -1039,7 +1039,7 @@ class PtTransformer0(nn.Module):
                 # if False:
                     # 0 1 2 3 4 5
                     a = [1,2,4,8,16,32,64,128]
-                    b = 1
+                    b = 0
                     c = 1
                     e = 1
                     L = 5
@@ -1051,8 +1051,8 @@ class PtTransformer0(nn.Module):
                         prob = out_probs[min(i+b, L)-j].squeeze(1)
                         stride_j = a[min(i+b, L)-j]
 
-                        # lr = prob[:,0].max()-prob[:,0].min()+1e-6
-                        # rr = prob[:,1].max()-prob[:,1].min()+1e-6
+                        lr = prob[:,0].max()-prob[:,0].min()+1e-6
+                        rr = prob[:,1].max()-prob[:,1].min()+1e-6
 
                         t = min(i+b+1, L+1)
 
@@ -1072,7 +1072,7 @@ class PtTransformer0(nn.Module):
                                 prob_left = prob[left_idx[left_mask], 0]
                                 # print(seg_left[left_mask])
                                 # seg_left[left_mask] += (ref_left*stride_j/c) * (1-pred_prob[left_mask])*(prob_left)
-                                seg_left[left_mask] += (ref_left*stride_i/c)
+                                seg_left[left_mask] += (ref_left*stride_i/c)*(prob_left/lr)
                                 
                                 # seg_left[left_mask] += (ref_left*stride_i/c) * (1-pred_prob[left_mask]/pred_prob_len)
                                 
@@ -1082,7 +1082,7 @@ class PtTransformer0(nn.Module):
                                 ref_right = ref[right_idx[right_mask], 1]  # todo
                                 prob_right = prob[right_idx[right_mask], 1] 
                                 # seg_right[right_mask] += (ref_right*stride_j/c) * (1-pred_prob[right_mask])*(prob_right)
-                                seg_right[right_mask] += (ref_right*stride_i/c)
+                                seg_right[right_mask] += (ref_right*stride_i/c)*(prob_right/rr)
                                 # seg_right[right_mask] += (ref_right*stride_j/c) * (1-pred_prob[right_mask]/pred_prob_len)
 
                                 # pred_prob[right_mask] *= torch.max((1.05 - pred_prob[right_mask]), 
