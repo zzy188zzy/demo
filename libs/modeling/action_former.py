7332877bde3fd915e287b14dbfb18b0d5fda6193
fy-coder <76418928+fy-coder@users.noreply.github.com>
2023-02-04 00:08:58 +0800

    Update action_former.py

--- libs/modeling/action_former.py
+++ libs/modeling/action_former.py
@@ -1066,7 +1066,7 @@ class PtTransformer0(nn.Module):
                                 ref_left = ref[left_idx[left_mask], 0]  # todo
                                 prob_left = prob[left_idx[left_mask], 0]
                                 # print(seg_left[left_mask])
-                                seg_left[left_mask] += (ref_left*stride_j/c) * (1 - pred_prob[left_mask])*prob_left
+                                seg_left[left_mask] += (ref_left*stride_j/c) * ((1 - pred_prob[left_mask])+prob_left)
                                 # seg_left[left_mask] += (ref_left*stride_i/c)
                                 # seg_left[left_mask] += (ref_left*stride_i/c) * (pred_prob[left_mask])
                                 
@@ -1075,7 +1075,7 @@ class PtTransformer0(nn.Module):
 
                                 ref_right = ref[right_idx[right_mask], 1]  # todo
                                 prob_right = prob[right_idx[right_mask], 1] 
-                                seg_right[right_mask] += (ref_right*stride_j/c) * (1 - pred_prob[right_mask])*prob_right
+                                seg_right[right_mask] += (ref_right*stride_j/c) * ((1 - pred_prob[right_mask])+prob_right)
                                 # seg_right[right_mask] += (ref_right*stride_i/c)
                                 # seg_left[left_mask] += (ref_left*stride_i/c) * (pred_prob[left_mask])
 
