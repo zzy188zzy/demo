fb7f815f952a26dddbdf29e528441490a369b56a
fy-coder <76418928+fy-coder@users.noreply.github.com>
2023-02-04 14:31:45 +0800

    Update action_former.py

--- libs/modeling/action_former.py
+++ libs/modeling/action_former.py
@@ -1032,10 +1032,10 @@ class PtTransformer0(nn.Module):
 
                 pred_prob_len = max(pred_prob.max()-pred_prob.min(), 0.5)
 
-                use_round = True
-                if i!=0 and i!=1 and i!=2:
+                use_round = True  # -0.02 +0.04 -0.2
+                if i!=0 and i!=1 and i!=2 and i!=3:
                 # if i!=0 :
-                # if True:
+                # if True:  
                 # if False:
                     # 0 1 2 3 4 5
                     a = [1,2,4,8,16,32,64,128]
