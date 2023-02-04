96d506307872de94e67dc94aaaa865e03f917378
fy-coder <76418928+fy-coder@users.noreply.github.com>
2023-02-04 18:00:49 +0800

    Update action_former.py

--- libs/modeling/action_former.py
+++ libs/modeling/action_former.py
@@ -1034,8 +1034,8 @@ class PtTransformer0(nn.Module):
 
                 use_round = True  # -0.02 +0.04 -0.2 -0.56 -0.05 -0.02
                 # if i!=0 and i!=1 and i!=2 and i!=3 and i!=4 and i!=5:
-                if i==3:
-                # if True:  
+                # if i==3:
+                if True:  
                 # if False:
                     # 0 1 2 3 4 5
                     a = [1,2,4,8,16,32,64,128]
@@ -1043,7 +1043,7 @@ class PtTransformer0(nn.Module):
                     c = 1
                     e = 1
                     L = 5  # 5
-                    f = -1  # 1
+                    f = 1  # 1
                     stride_i = a[min(i+b, L)]
                     s_i = a[i]
                     for j in range(min(i+b+f, L+f)):  # 1 2 3 4 5 6
