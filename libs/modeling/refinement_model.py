4df25ab6485641e08f896508cc551c029b590f7c
fy-coder <76418928+fy-coder@users.noreply.github.com>
2023-02-04 20:38:14 +0800

    Update refinement_model.py

--- libs/modeling/refinement_model.py
+++ libs/modeling/refinement_model.py
@@ -207,9 +207,9 @@ class Refinement_module(nn.Module):
                 inf_loss.append(loss['inf_loss'])
                 prob_loss.append(loss['prob_loss'])
 
-            ref_loss = torch.stack(ref_loss).min()*2
+            ref_loss = torch.stack(ref_loss).min()*4  # 2
             inf_loss = torch.stack(inf_loss).min()*0
-            prob_loss = torch.stack(prob_loss).min()*0.5
+            prob_loss = torch.stack(prob_loss).min()*1  # 0.5
             final_loss = ref_loss  + prob_loss
 
             return {
